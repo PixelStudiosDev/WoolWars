dbb00443c748a27b237a87f19a97fe781b59c36b
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2022-03-28 16:54:47 +0200

    Initial commit

---
+++ src/main/java/me/cubecrafter/woolwars/WoolWars.java
@@ -0,0 +1,17 @@
+package me.cubecrafter.woolwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WoolWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
