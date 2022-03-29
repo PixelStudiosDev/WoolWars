ac0820b0245629cd8be229b0ca00463f8dc3d6c7
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2022-03-29 20:43:50 +0200

    :D

--- src/main/java/me/cubecrafter/woolwars/utils/GameUtil.java
+++ src/main/java/me/cubecrafter/woolwars/utils/GameUtil.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 public class GameUtil {
 
     public static Arena getArenaByPlayer(Player player) {
+        //
         for (Arena arena : WoolWars.getInstance().getGameManager().getArenas()) {
             if (arena.getPlayers().contains(player)) {
                 return arena;
