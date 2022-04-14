6ee3e25e43cb6b1d87839a9f23f514702e71b50d
Infinity <xa583179@gmail.com>
2022-04-14 20:20:00 +0530

    Started Work on Arena Setup System

---
+++ src/main/java/me/cubecrafter/woolwars/setup/SetupHandler.java
@@ -0,0 +1,36 @@
+package me.cubecrafter.woolwars.setup;
+
+import lombok.experimental.UtilityClass;
+import me.cubecrafter.woolwars.WoolWars;
+import me.cubecrafter.woolwars.core.Arena;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Infinity
+ * 14-04-2022 / 04:58 PM
+ * WoolWars / me.cubecrafter.woolwars.setup
+ */
+
+@UtilityClass
+public class SetupHandler {
+
+    private File arenaFile;
+    private WoolWars instance = WoolWars.getInstance();
+    private YamlConfiguration configuration;
+
+    public void createArena(String arenaName, int maxTeamPlayers, int minPlayers) throws IOException {
+        arenaFile = new File(WoolWars.getInstance().getDataFolder() + File.pathSeparator + "arenas", arenaName + ".yml");
+        if (!arenaFile.exists()){
+            instance.saveResource(arenaName + ".yml", false);
+            configuration = YamlConfiguration.loadConfiguration(arenaFile);
+        }
+        Arena arena = new Arena(arenaName, configuration);
+        configuration.set("displayname", arenaName);
+        configuration.set("max-players-per-team", maxTeamPlayers);
+        configuration.set("min-players", minPlayers);
+        configuration.save(arenaFile);
+    }
+}
---
+++ src/main/java/me/cubecrafter/woolwars/setup/SetupManager.java
@@ -0,0 +1,12 @@
+package me.cubecrafter.woolwars.setup;
+
+/**
+ * @author Infinity
+ * 14-04-2022 / 04:57 PM
+ * WoolWars / me.cubecrafter.woolwars.setup
+ */
+
+public class SetupManager {
+
+
+}
